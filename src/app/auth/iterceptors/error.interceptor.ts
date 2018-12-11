e746aaf4fb9dd7626956725e4db8102d7d14d88d
przemekpobuta <przemyslawpobuta@gmail.com>
2018-12-11 23:10:35 +0100

    Repair "har-validator"

--- src/app/auth/iterceptors/error.interceptor.ts
+++ src/app/auth/iterceptors/error.interceptor.ts
@@ -3,15 +3,19 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from '../auth.service';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
       if (err.status === 401) {
         // auto logout if 401 response returned from api
+        this.router.navigate(['/']);
         this.authService.logout();
 
         // not tested in secured route
