5756bdae5f9f744a2afb0cb3ed57d8051768120c
przemekpobuta <przemyslawpobuta@gmail.com>
2018-12-11 22:52:06 +0100

    Repair "har-validator"

---
+++ src/app/sites/panel/files/file-explorer/modals/move-dialog/move-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-move-dialog',
+  templateUrl: './move-dialog.component.html',
+  styleUrls: ['./move-dialog.component.scss']
+})
+export class MoveDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
