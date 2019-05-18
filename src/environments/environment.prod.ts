b70fc665a8b9d103abc8da3ef148f40800662967
Przemek Pobuta <przemyslawpobuta@gmail.com>
2019-05-18 17:37:39 +0200

    Repaired prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // adres URL API na produkcji (AWS)
-  api_url: 'http://91.246.73.79:60011/api/',
+  api_url: 'http://52.178.36.176:8000/api/',
   maxFileSize: 19000000
 };
